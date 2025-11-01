04d40b3637f4a23fcef9767b83e8bc18f6886db9
Neha Sharma <neha.iitjdh@gmail.com>
2025-11-02 00:09:26 +0530

    size changed

--- src/assets/icons/SortIcons.jsx
+++ src/assets/icons/SortIcons.jsx
@@ -1,15 +1,14 @@
-// Sort arrow icons - INCREASED SIZE
 export const SortUpIcon = ({ className = "" }) => (
   <svg
-    width="16" // Changed from 12 to 16
-    height="16" // Changed from 12 to 16
-    viewBox="0 0 16 16" // Changed from "0 0 12 12"
+    width="16"
+    height="16" 
+    viewBox="0 0 16 16" 
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     className={className}
   >
     <path
-      d="M8 4L12 10H4L8 4Z" // Scaled path from 6,3,9,7,3 to 8,4,12,10,4
+      d="M8 4L12 10H4L8 4Z" 
       fill="currentColor"
     />
   </svg>
@@ -17,15 +16,15 @@ export const SortUpIcon = ({ className = "" }) => (
 
 export const SortDownIcon = ({ className = "" }) => (
   <svg
-    width="16" // Changed from 12 to 16
-    height="16" // Changed from 12 to 16
-    viewBox="0 0 16 16" // Changed from "0 0 12 12"
+    width="16" 
+    height="16" 
+    viewBox="0 0 16 16" 
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     className={className}
   >
     <path
-      d="M8 12L4 6H12L8 12Z" // Scaled path from 6,9,3,5,9 to 8,12,4,6,12
+      d="M8 12L4 6H12L8 12Z" 
       fill="currentColor"
     />
   </svg>
@@ -33,9 +32,9 @@ export const SortDownIcon = ({ className = "" }) => (
 
 export const SortIcon = ({ className = "" }) => (
   <svg
-    width="16" // Changed from 12 to 16
-    height="18" // Changed from 14 to 18
-    viewBox="0 0 16 18" // Changed from "0 0 12 14"
+    width="16"
+    height="18" 
+    viewBox="0 0 16 18" 
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     className={className}
